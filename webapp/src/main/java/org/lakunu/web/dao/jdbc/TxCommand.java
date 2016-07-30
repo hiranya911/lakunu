bce7ef88728a1a2bb27676d16771acfdd8da1587
hiranya <hiranya911@gmail.com>
2016-07-30 16:56:00 -0700

    Cleaned up TxCommand class

--- webapp/src/main/java/org/lakunu/web/dao/jdbc/TxCommand.java
+++ webapp/src/main/java/org/lakunu/web/dao/jdbc/TxCommand.java
@@ -6,13 +6,23 @@ import java.sql.SQLException;
 
 public abstract class TxCommand<T> extends Command<T> {
 
+    protected final int txIsolationLevel;
+
     protected TxCommand(DataSource dataSource) {
+       this(dataSource, -1);
+    }
+
+    protected TxCommand(DataSource dataSource, int txIsolationLevel) {
         super(dataSource);
+        this.txIsolationLevel = txIsolationLevel;
     }
 
     @Override
     protected final T doRun(Connection connection) throws SQLException {
         connection.setAutoCommit(false);
+        if (txIsolationLevel != -1) {
+            connection.setTransactionIsolation(txIsolationLevel);
+        }
         try {
             T result = doTransaction(connection);
             connection.commit();
@@ -20,6 +30,8 @@ public abstract class TxCommand<T> extends Command<T> {
         } catch (Exception ex) {
             connection.rollback();
             throw ex;
+        } finally {
+            connection.setAutoCommit(true);
         }
     }
 
